f9ec592c49cc38492b4953aef76df8ac75cc17ea
Arif <dicodingreviewer@gmail.com>
2019-08-16 14:30:52 +0700

    Update AndroidX MyIntentApp

---
+++ MyIntentApp/app/src/androidTest/java/com/dicoding/myintentapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.dicoding.myintentapp
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        assertEquals("com.dicoding.myintentapp", appContext.packageName)
+    }
+}
