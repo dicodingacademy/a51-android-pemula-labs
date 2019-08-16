f893afa3523b3d7fc7b1e245bc3e272b6d23c64e
Arif <dicodingreviewer@gmail.com>
2019-08-16 11:39:07 +0700

    Update AndroidX MyIntentApp

--- MyIntentApp/app/src/main/java/com/dicoding/myintentapp/MoveActivity.java
+++ MyIntentApp/app/src/main/java/com/dicoding/myintentapp/MoveActivity.java
@@ -1,8 +1,7 @@
 package com.dicoding.myintentapp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MoveActivity extends AppCompatActivity {
 
