d67926a568fba71e2ae6f41f6cf0ec4e80a7dc20
Arif <dicodingreviewer@gmail.com>
2019-08-16 13:42:54 +0700

    Update AndroidX RecyclerView

--- MyRecyclerView/app/src/main/java/com/dicoding/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/dicoding/myrecyclerview/MainActivity.java
@@ -1,10 +1,10 @@
 package com.dicoding.myrecyclerview;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
