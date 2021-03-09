a4ec3b6c15db8051eede792f7c40a2fbcdd3c08a
Arif <faizin.arief@gmail.com>
2021-03-09 17:02:00 +0700

    Remove Switch Case for AGP 5

--- MyIntentApp/app/src/main/java/com/dicoding/myintentapp/MainActivity.java
+++ MyIntentApp/app/src/main/java/com/dicoding/myintentapp/MainActivity.java
@@ -26,24 +26,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_move_activity:
-                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
-                startActivity(moveIntent);
-                break;
-
-            case R.id.btn_move_activity_data:
-                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
-                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
-                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
-                startActivity(moveWithDataIntent);
-                break;
-
-            case R.id.btn_dial_number:
-                String phoneNumber = "081210841382";
-                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
-                startActivity(dialPhoneIntent);
-                break;
+        if (v.getId() == R.id.btn_move_activity) {
+            /*
+            Intent untuk memulai activity baru
+            */
+            Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+            startActivity(moveIntent);
+        } else if (v.getId() == R.id.btn_move_activity_data) {
+            /*
+            Intent untuk mengirimkan data ke activity lain
+            */
+            Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
+            startActivity(moveWithDataIntent);
+        } else if (v.getId() == R.id.btn_dial_number) {
+            /*
+            Intent action untuk menjalankan action dial
+            */
+            String phoneNumber = "081210841382";
+            Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+            startActivity(dialPhoneIntent);
         }
     }
 }
