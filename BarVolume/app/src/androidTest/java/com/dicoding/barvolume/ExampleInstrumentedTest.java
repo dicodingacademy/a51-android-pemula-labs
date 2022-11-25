19592410e0b2b0493112a11b8b3c6370e54ca9f5
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-11-25 17:58:54 +0700

    update gradle to 7.4

--- BarVolume/app/src/androidTest/java/com/dicoding/barvolume/ExampleInstrumentedTest.java
+++ BarVolume/app/src/androidTest/java/com/dicoding/barvolume/ExampleInstrumentedTest.java
@@ -1,7 +1,9 @@
 package com.dicoding.barvolume;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -14,12 +16,12 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.dicoding.barvalume", appContext.getPackageName());
     }
