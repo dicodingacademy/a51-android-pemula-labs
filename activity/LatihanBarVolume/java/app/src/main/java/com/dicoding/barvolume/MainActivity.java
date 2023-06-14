8d4a4765ed0b07b42838d76605efca03bacf0a65
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2023-06-14 16:33:40 +0700

    Update Intent Java and Kotlin

--- activity/LatihanBarVolume/java/app/src/main/java/com/dicoding/barvolume/MainActivity.java
+++ activity/LatihanBarVolume/java/app/src/main/java/com/dicoding/barvolume/MainActivity.java
@@ -1,4 +1,4 @@
-package com.dicoding.picodiploma.barvolume;
+package com.dicoding.barvolume;
 
 import android.os.Bundle;
 
--- activity/LatihanBarVolume/kotlin/app/src/main/java/com/dicoding/barvolume/MainActivity.kt
+++ activity/LatihanBarVolume/kotlin/app/src/main/java/com/dicoding/barvolume/MainActivity.kt
@@ -1,14 +1,13 @@
-package com.dicoding.picodiploma.barvolume
+package com.dicoding.barvolume
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
     private lateinit var edtWidth: EditText
     private lateinit var edtHeight: EditText
     private lateinit var edtLength: EditText
@@ -42,8 +41,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         outState.putString(STATE_RESULT, tvResult.text.toString())
     }
 
-    override fun onClick(v: View) {
-        if (v.id == R.id.btn_calculate) {
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_calculate) {
             val inputLength = edtLength.text.toString().trim()
             val inputWidth = edtWidth.text.toString().trim()
             val inputHeight = edtHeight.text.toString().trim()
