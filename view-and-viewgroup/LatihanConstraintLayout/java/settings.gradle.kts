8420e4230642ee285e5f9958040d1663ad8dcd29
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2023-07-27 12:40:49 +0700

    Update Latihan View and View Group to Android Studio Giraffe

--- view-and-viewgroup/LatihanConstraintLayout/java/settings.gradle.kts
+++ view-and-viewgroup/LatihanConstraintLayout/java/settings.gradle.kts
@@ -12,5 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "MyViewAndViewGroup"
-include ':app'
+rootProject.name = "MyConstraintView"
+include(":app")
--- view-and-viewgroup/LatihanViewAndViewGroup/java/settings.gradle.kts
+++ view-and-viewgroup/LatihanViewAndViewGroup/java/settings.gradle.kts
@@ -12,5 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "MyConstraintView"
-include ':app'
+rootProject.name = "MyViewAndViewGroup"
+include(":app")
