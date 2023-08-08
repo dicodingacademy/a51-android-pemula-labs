7a49a784c03b68f45ec75cc1e42b55d939cd9219
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2023-08-08 12:52:38 +0700

    Update Stye and Theme App

---
+++ style-and-theme/LatihanStyleAndTheme/kotlin/app/src/main/java/com/dicoding/myviewandviewgroup/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dicoding.myviewandviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Google Pixel"
+
+    }
+}
